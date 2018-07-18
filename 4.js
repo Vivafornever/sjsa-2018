b63f76973690011d741811f8f9e5248e9d1b45bb
Vivafornever <39170248+Vivafornever@users.noreply.github.com>
2018-07-18 20:58:20 +0200

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,89 @@
+ //function nameOFFunction(parameter1, param2){
+ 	// //naredbi
+ // 	//}
+ // function firstFunction() {
+ // alert("Hello world")
+ // }
+
+ // firstFunction();
+
+ // function multiplier(num1, num2){
+ // 	return num1*num2;
+ // }
+ // console.log(multiplier(3,4));
+
+ // function greeting(name, age){
+ // 	return "jas sum "+name+" i imam "+age+" godina.";
+ // }
+ // console.log(greeting("Magdi",31));
+
+ // // greeting("Magdi",31); 
+
+ // console.log(Math.pow(2,3));
+
+ // 
+
+ // function power (base, exponent) {
+ // 	var result = 1;
+ // 	for(var i = 0; i<exponent; i ++){
+ // 		result*=base;
+ // 	}
+ // 	return result;	 	
+ // }
+
+ // console.log(power(2,3));
+
+//   function power (base, exponent) {
+//  	var result = base;
+//  	for(var i = 0; i<exponent-1; i ++){
+//  		result*=base;
+//  	}
+//  	return result;	 	
+//  }
+
+//  console.log(power(2,3));
+
+// var test = function(){
+// 	return "From a variable";
+//  }
+
+// console.log(test());
+
+//vgnezdeni funkcii
+
+// var landscape = function(){
+
+// 	var result = "";
+
+// 	var flat = function(size){
+
+// 		for (var i = 0;i <size; i++){
+// 			result+="_";
+// 		}
+// 	}
+// 	var mountain = function(size){
+// 		result+="/";
+// 		for(var i = 0; i < size; i++){
+// 			result+="'";
+// 		}
+// 		result+="\\"
+// 	}
+
+// 	flat(4);
+// 	mountain(3);
+// 	flat(4);
+// 	mountain(3);
+// 	flat(4);
+
+// 	return result;
+// }
+// console.log(landscape());
+
+//primer : 
+
+// function future(){
+// // 	return "We don't have flying car";
+// // }
+// // console.log("The future says", future());  1 
+
+// // console.log("The future says " +future());  2
